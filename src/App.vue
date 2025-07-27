800fd29873a6e6078d640243263c67339107db73
narges.karimian <nargeskarimian1551@gmail.com>
2025-07-27 13:15:58 +0330

    remove all

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       :header="popoutStore.popoutHeader"
       :message="popoutStore.popoutMessage"
       :buttons="['ok']"
-      @didDismiss="() => popoutStore.isPopoutOpen = false"
+      @didDismiss="() => (popoutStore.isPopoutOpen = false)"
       :is-open="popoutStore.isPopoutOpen"
       cssClass="my-custom-class"
     ></ion-alert>
@@ -13,16 +13,17 @@
 </template>
 
 <script setup lang="ts">
-import { CapacitorUpdater } from '@capgo/capacitor-updater'
-import { IonApp, IonRouterOutlet, IonAlert } from '@ionic/vue';
-import { usePopoutStore } from './popoutStore';
-const popoutStore = usePopoutStore()
+// import { CapacitorUpdater } from '@capgo/capacitor-updater'
+import { IonApp, IonRouterOutlet, IonAlert } from "@ionic/vue";
+import { usePopoutStore } from "./popoutStore";
+const popoutStore = usePopoutStore();
 
-CapacitorUpdater.notifyAppReady()
+// CapacitorUpdater.notifyAppReady()
 
 // await new Promise(r => setTimeout(r, 2000));
 // await CapacitorUpdater.setMultiDelay({
 //   delayConditions: [ { kind: "kill" } ]
 // })
 // console.log('RESOLVED')
-</script>>
+</script>
+>
