883d8130356cef1cbfe92b9f424201d6456d9381
narges.karimian <nargeskarimian1551@gmail.com>
2025-07-28 16:02:32 +0330

    fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.demo.app',
+  appId: 'anardoni.export',
   appName: 'capgoTestVueApp',
   webDir: 'dist',
   server: {
